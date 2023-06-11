34708923eb8c8b642e788e4703fcb73b4072612c
grigoryanhrant <hgrigoryan755@gmail.com>
2023-06-11 22:13:05 +0400

    update

--- creational/_singleton/singleton.js
+++ creational/_singleton/singleton.js
@@ -1,5 +1,4 @@
 class Counter {
-
 	constructor() {
 		if (typeof Counter.instance === 'object') {
 			return Counter.instance;
@@ -8,12 +7,18 @@ class Counter {
 		Counter.instance = this;
 		return this;
 	}
-  
+
 	getCount() {
 		return this.count;
 	}
-  
+
 	increaseCount() {
 		return this.count++;
 	}
 }
+
+const myCount1 = new Counter();
+const myCount2 = new Counter();
+
+myCount1.increaseCount();
+myCount2.increaseCount();
